8dad42064cc7790d6ada36cf60f89a90a19998c3
Havale <shavale@lm-bng-00668401.paypalcorp.com>
2017-03-31 09:49:18 +0530

    ReadMe update

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
+
+This project is for to get list of todo's and search by name and open project details, these detail now coming from mock data.
+
 ``` clone repo from
-	clone
+	clone git@github.com:sachinmjr/projecttodolist.git
 
 	cd projecttodolist
 
 	npm install
 
 	npm start
-	
+
 ```
--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'about',
   template: `
-  	<h3>This project is for to get list of todo's and search by name and open project details, these detail now coming by mock data </h3>`,
+  	<h3>This project is for to get list of todo's and search by name and open project details, these detail now coming from mock data. </h3>`,
 })
 export class AboutComponent  {}
